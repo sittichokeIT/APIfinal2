d0c62fba078cdf3e956879b995b1c8f872f7cba0
MoD_MoS <s62040626260097@email.kmutnb.ac.th>
2022-05-25 01:33:55 +0700

    Mos

--- server/controllers/user_control.js
+++ server/controllers/user_control.js
@@ -1,5 +1,8 @@
 const { response } = require('express')
 const user = require('../models/user_model')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const auth = require('../middleware/auth')
 
 const index = (req,res,next)=>{
     user.find({},(err,users) =>{
@@ -8,76 +11,80 @@ const index = (req,res,next)=>{
     })
 }
 
-const createStudent = async (req,res) => {
+const createStudent = async (req, res) => {
+    let Password = await bcrypt.hash(req.body.password, 10)
     let users = await user({
         UserID: req.body.UserID,
         status: "Student",
         firstname: req.body.firstname,
         lastname: req.body.lastname,
         email: req.body.email,
-        password: req.body.password,
+        password: Password,
         address: req.body.address,
-        telno: req.body.telno
+        telno: req.body.telno,
     })
     users.save()
-    .then(response => {
-        res.json({
-            message: 'Student added Successfully!!'
+        .then(response => {
+            res.json({
+                message: 'Student added Successfully!'
+            })
         })
-    })
-    .catch(err => {
-        res.json({
-            message: 'Student added Error!'
+        .catch(err => {
+            res.json({
+                message: 'Student added Error!'
+            })
         })
-    })
 }
 
-const createTeacher = (req,res) => {
+const createTeacher = async (req, res) => {
+    let Password = await bcrypt.hash(req.body.password, 10)
     let users = new user({
         UserID: req.body.UserID,
         status: "Teacher",
         firstname: req.body.firstname,
         lastname: req.body.lastname,
         email: req.body.email,
-        password: req.body.password,
+        password: Password,
         address: req.body.address,
-        telno: req.body.telno
+        telno: req.body.telno,
     })
     users.save()
-    .then(response => {
-        res.json({
-            message: 'Teacher added Successfully!'
+        .then(response => {
+            res.json({
+                message: 'Teacher added Successfully!'
+            })
         })
-    })
-    .catch(err => {
-        res.json({
-            message: 'Teacher added Error!'
+        .catch(err => {
+            res.json({
+                message: 'Teacher added Error!'
+            })
         })
-    })
 }
 
-const createLeader = (req,res) => {
+const createLeader = async (req, res) => {
+    let Password = await bcrypt.hash(req.body.password, 10)
     let users = new user({
         UserID: req.body.UserID,
         status: "Leader",
         firstname: req.body.firstname,
         lastname: req.body.lastname,
         email: req.body.email,
-        password: req.body.password,
+        password: Password,
         address: req.body.address,
-        telno: req.body.telno
+        telno: req.body.telno,
+        token: token
     })
     users.save()
-    .then(response => {
-        res.json({
-            message: 'Leader added Successfully!'
+        .then(response => {
+            res.json({
+                message: 'Leader added Successfully!'
+            })
         })
-    })
-    .catch(err => {
-        res.json({
-            message: 'Leader added Error!'
+        .catch(err => {
+            res.json({
+                message: 'Leader added Error!'
+            })
         })
-    })
 }
 
 const DeleteUser = async (req,res) => {
@@ -102,32 +109,29 @@ const login = async (req, res, next) => {
         console.log(userSearch.password)
         let Passwordcheck = await bcrypt.compare(req.body.password, userSearch.password)
         if (Passwordcheck) {
-            if (userSearch.token) {
-                res.header('auth-token', userSearch.token)
-                req.body.token = userSearch.token
-                auth.verifyToken(req, res, next)
-            } else {
-                let UserID = req.body.UserID
-                let token = jwt.sign(
-                    { UserID: user._id, UserID },
-                    process.env.TOKEN_KEY,
-                    { expiresIn: "1h" }
-                )
-                let data = {
-                    token: token
-                }
-                //console.log(token)
-                user.findOneAndUpdate({ UserID }, { $set: data })
-                    .then(() => {
-                        res.header('auth-token', token).send("Login sccesss")
-                    })
-                    .catch(err => {
-                        res.json({
-                            message: 'error'
-                        })
-                    })
+            let UserID = req.body.UserID
+            let token = jwt.sign(
+                { UserID: user._id, UserID },
+                process.env.TOKEN_KEY,
+                { expiresIn: "1h" }
+            )
+            let data = {
+                token: token
             }
-        } else {
+            //console.log(token)
+            user.findOneAndUpdate({ UserID }, { $set: data })
+                .then(() => {
+                    let status = userSearch.status
+                    res.json({
+                        message: status
+                    })
+                })
+                .catch(err => {
+                    res.json({
+                        message: 'error'
+                    })
+                })
+            }else {
             res.json({
                 message: "Login fail"
             })
--- server/routes/user_route.js
+++ server/routes/user_route.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const usercontrol = require('../controllers/user_control')
+const auth = require('../middleware/auth')
 
 router.get('/',usercontrol.index)
 router.post('/createStudent',usercontrol.createStudent)
